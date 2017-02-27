0edbdd44e8c02df5d36ccb9736252027f392356c
brad1 <brad1@users.noreply.github.com>
2017-02-26 20:55:27 -0500

    Update zsh.rb

--- recipes/zsh.rb
+++ recipes/zsh.rb
@@ -1,28 +1,28 @@
 package "zsh"
 
-homedir = "/home/#{node['development-setup']['user']}"
+homedir = "/home/#{node['development-setup']['user']['name']}"
 
 git "#{homedir}/.oh-my-zsh" do
   repository 'https://github.com/robbyrussell/oh-my-zsh.git'
-  user node['developer-setup']['user']
+  user node['development-setup']['user']['name']
   reference "master"
   action :sync
 end
 
 template "#{homedir}/.zshrc" do
   source "zshrc.erb"
-  owner node['developer-setup']['user']
+  owner node['development-setup']['user']['name']
   mode "644"
   action :create_if_missing
   variables({
-    :user           => node['developer-setup']['user'],
+    :user           => node['development-setup']['user']['name'],
     :theme          => 'robbyrussell',
     :case_sensitive => false,
     :plugins        => %w{git}
   })
 end
 
-user node['development-setup']['user'] do
+user node['development-setup']['user']['name'] do
   action :modify
   shell '/bin/zsh'
 end
