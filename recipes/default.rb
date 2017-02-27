aacb8f75bd4cda93ab6d2094a5a0df7b9e74d54f
brad1 <brad1@users.noreply.github.com>
2017-02-26 20:35:38 -0500

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,12 @@
 case node['platform_family']
 when 'arch'
-  include_recipe 'developer-setup::arch'
+  include_recipe 'development-setup::arch'
 when 'rhel'
   Chef::Log.info("no rhel")
 when 'mac_os_x'
-  include_recipe 'developer-setup::osx'
+  include_recipe 'development-setup::osx'
 when 'windows'
-  include_recipe 'developer-setup::windows'
+  include_recipe 'development-setup::windows'
 else
   Chef::Log.fatal("Don't know what to do with platform family #{node['platform_family']}")
 end
