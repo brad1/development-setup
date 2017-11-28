a981eb0dc6980a3412a9c42e4ea939b1cf1a8955
brad1 <brad1@users.noreply.github.com>
2017-11-28 11:56:47 -0500

    Update setup.rb

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,4 +1,4 @@
-include_recipe 'development-setup::user'
+include_recipe 'development-setup::user' if node['user']['enabled']
 include_recipe 'development-setup::zsh'
 include_recipe 'development-setup::clients'
 include_recipe 'development-setup::build_system'
