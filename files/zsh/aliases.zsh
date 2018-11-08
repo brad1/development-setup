5d0b43a7ea69619ba04fd41ab8dccb309e7fdef7
Brad Yohe <byohe@valcom.com>
2018-11-08 12:00:22 -0500

    update

--- files/zsh/aliases.zsh
+++ files/zsh/aliases.zsh
@@ -1,8 +1,12 @@
 # [new]
-alias cd_devsetup="cd $DEVSETUP"
-alias zsh_cheatsheet='cat /opt/chef/cookbooks/development-setup/files/zsh/cheatsheet'
+alias git_time_back='git log --grep Merge | grep Date -B5 | head -n200 | tail -n4'
+alias vim_timesheet='vim ~/Documents/vim/topics/planning/timesheet/log'
+alias vim_timesheet_status='vim ~/Documents/vim/topics/planning/timesheet/status-update'
+alias new_grep='ag -o "[0-9]+"' # fast
+alias fzf_search='fzf' # fast fuzzy file search
+alias zsh_noscm='vim ~/.noscm.zsh'
+alias zsh_sources='vim /opt/chef/cookbooks/development-setup/files/zsh/sources.zsh'
 alias zsh_controls='cat /opt/chef/cookbooks/development-setup/files/zsh/functions.zsh'
-alias tmux_cheatsheet='cat /opt/chef/cookbooks/development-setup/files/tmux/cheatsheet'
 alias mount_iso='mount -t iso9660 -o loop /home/brad/DVD.iso /mnt/iso/'
 # sed -i '/pattern to match/d' ./infile
 alias vbox_screenshot_to_text='VBoxManage controlvm vmass screenshotpng screenshot.png && convert -colorspace gray -fill white -resize 600% -sharpen 0x2 screenshot.png screenshot.jpg && tesseract screenshot.jpg ocr && cat ocr.txt'
@@ -18,15 +22,30 @@ alias swap_files_remove='rm $(swap_files|xargs)'
 alias osx_ram='sysctl -a | grep hw.memsize'
 alias group_list='getent group wheel'
 alias known_hosts='vim ~/.ssh/known_hosts'
+alias grep_extended='grep -Eo [0-9]+'
+alias clipboard='vim ~/Documents/vim/clipboard.txt'
 
+# [cheatsheets]
+alias cheatsheet_virtualbox='view /opt/chef/cookbooks/development-setup/files/cheatsheets/virtualbox.txt'
+alias cheatsheet_postgres='view /opt/chef/cookbooks/development-setup/files/cheatsheets/postgres'
+alias cheatsheet_vim='view /opt/chef/cookbooks/development-setup/files/cheatsheets/vim'
+alias cheatsheet_qwerty='cat /opt/chef/cookbooks/development-setup/files/cheatsheets/qwerty'
+alias cheatsheet_zsh='cat /opt/chef/cookbooks/development-setup/files/zsh/cheatsheet'
+alias cheatsheet_bash='vim /opt/chef/cookbooks/development-setup/files/bash/cheatsheet'
+alias cheatsheet_bash_commands='cat /opt/chef/cookbooks/development-setup/files/cheatsheets/bash_commands'
+alias cheatsheet_tmux='cat /opt/chef/cookbooks/development-setup/files/tmux/cheatsheet'
+alias cheatsheet_ranger='echo S open shell in directory, c-h to show hidden files '
 
+# [find]
+alias list_files='find . -maxdepth 1 -type f'
 
 # [locations]
 
+alias cd_vim='cd ~/Documents/vim/links'
 alias cd_devsetup='cd /opt/chef/cookbooks/development-setup'
 alias cd_ib='cd ~/Projects/image_builder'
 alias cd_projects='cd ~/Projects'
-alias cd_prototypes='~/Documents/vim/prototypes'
+alias cd_prototypes='~/Documents/vim/scratch/prototypes'
 alias cd_vmass='cd ~/Projects/vmass'
 alias examples='open -a Finder ~/Examples'
 
@@ -70,6 +89,11 @@ alias install_homebrew='/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercon
 
 
 # [git/projects]
+alias git_clean='echo WIP: git clean -xdf --exclude=".vagrant*" --exclude="node_modules"'
+#alias git_clean='git clean -xf --exclude=".vagrant"'
+#alias git_clean='git clean -xdn --exclude=".vagrant*" --exclude="node_modules"'
+alias git_importfrombranch='git checkout release -- path/to/file'
+alias git_realclean='git clean -fdx'
 alias cp_iso='cp $(find . -name VE\*iso) /media/sf_Desktop'
 alias project_clean='git checkout .; git clean -f; make clean'
 #
@@ -94,7 +118,6 @@ alias vagrant_deathstar='vagrant halt --force; vagrant destroy --force'
 
 # [shell]
 alias tmp='vim ~/tmp/$(date "+%F-%T")'
-alias bash_cheatsheet='cat /opt/chef/cookbooks/development-setup/files/bash/cheatsheet'
 alias c="cut -d' ' -f"
 alias l='ls -lh'
 alias selinux_permissive='setenforce 0'
