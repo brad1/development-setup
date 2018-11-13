7cc9635811bf0996071a31778c1c3f9ddd734454
Brad Yohe <byohe@valcom.com>
2018-11-13 15:49:34 -0500

    update

--- files/zsh/functions.zsh
+++ files/zsh/functions.zsh
@@ -1,30 +1,6 @@
-# consider a new pattern:
-# thread readme # opens file
-# thread readme a new comment # appends to file
-# thread read...  # with autocomplete
-readme () {
-  cd ~/Documents/vim
-  vim raw/log
+vimm () {
+  vim $(find . -name \*$1\*)
 }
-
-#readme () {
-#  cd ~/Documents/vim/raw
-#  list=$(ls | grep -v collapse)
-#  terminal_height=$(tput lines)
-#  file_length=$(cat $(echo $list | tail -1) | wc -l)
-#  if [[ "$file_length" -gt "$terminal_height" ]]; then
-#    echo $(date +%Y%m%d) >> $(date +%Y%m%d)
-#  fi
-#  vim $(ls | grep -v collapse | xargs)
-#}
-
-# readme () {
-#  vim readme     # no
-#  vim **/readme  # maybe
-#  vim ~/Projects/**/readme # better
-#  vim ~/Documents/vim/raw/...  ~/Projects/**/readme # start with the global, than add others
-#  ...
-#}
 
 save () {
   revision="$(git log --oneline | head -n1)"
@@ -70,3 +46,32 @@ function down_widget() {
 }
 zle -N down_widget
 bindkey "^j" down_widget
+
+
+# consider a new pattern:
+# thread readme # opens file
+# thread readme a new comment # appends to file
+# thread read...  # with autocomplete
+readme () {
+  cd ~/Documents/vim
+  vim raw/log
+}
+
+#readme () {
+#  cd ~/Documents/vim/raw
+#  list=$(ls | grep -v collapse)
+#  terminal_height=$(tput lines)
+#  file_length=$(cat $(echo $list | tail -1) | wc -l)
+#  if [[ "$file_length" -gt "$terminal_height" ]]; then
+#    echo $(date +%Y%m%d) >> $(date +%Y%m%d)
+#  fi
+#  vim $(ls | grep -v collapse | xargs)
+#}
+
+# readme () {
+#  vim readme     # no
+#  vim **/readme  # maybe
+#  vim ~/Projects/**/readme # better
+#  vim ~/Documents/vim/raw/...  ~/Projects/**/readme # start with the global, than add others
+#  ...
+#}
