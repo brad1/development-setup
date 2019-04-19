f0e7dece17551d1dd59b2630a7ada4ccd7029c70
Brad Yohe <byohe@valcom.com>
2019-04-19 13:50:00 -0400

    update

--- files/zsh/sources.zsh
+++ files/zsh/sources.zsh
@@ -4,6 +4,7 @@ source $INCLUDE/aliases.zsh
 source $INCLUDE/ruby.zsh
 source $INCLUDE/variables.zsh
 source $INCLUDE/functions.zsh
+source $INCLUDE/history.zsh
 # source $INCLUDE/moveme.zsh # left over from before
 # source $INCLUDE/numpad.zsh
 
