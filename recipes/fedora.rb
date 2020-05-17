bbc4351b17d6a25a4d5fa2471ff33d74e9dbc32a
Brad Yohe <byohe@valcom.com>
2020-05-17 16:37:36 -0400

    Don't link to bin, other software uses

--- recipes/fedora.rb
+++ recipes/fedora.rb
@@ -3,6 +3,6 @@ users = 'Users' if node['platform_family'].eql? 'mac_os_x'
 
 homedir = "/#{users}/#{node['development-setup']['user']['name']}"
 
-link "#{homedir}/bin" do
-  to "/opt/chef/cookbooks/development-setup/files/bin"
-end
+#link "#{homedir}/bin" do
+#  to "/opt/chef/cookbooks/development-setup/files/bin"
+#end
