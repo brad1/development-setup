24b6cc6919dea812e19356ac6ed319f383cecf17
Brad Yohe <byohe@valcom.com>
2022-07-06 09:23:36 -0400

    vim - add tagbar

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -26,6 +26,7 @@ end
  "https://github.com/garbas/vim-snipmate.git",
  "https://github.com/ntpeters/vim-better-whitespace.git",
  "https://github.com/Raimondi/delimitMate.git",
+ "https://github.com/preservim/tagbar.git",
  "https://github.com/honza/vim-snippets.git"].each do |repo_url|
 
   repo_name = repo_url.split('/').last.split('.')
